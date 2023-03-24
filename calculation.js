44e6e68df390d3f607c0579822c5769b5092aed8
hupalex <alex.hupuczi@gmail.com>
2023-03-24 23:42:26 +0100

    Create function that populates pressed buttons on the display, further CSS improvements added also

--- calculation.js
+++ calculation.js
@@ -2,6 +2,8 @@ let firstNumber;
 let operator;
 let secondNumber;
 
+let displayValue
+
 function add(a,b){
     return a+b;
 }
@@ -26,4 +28,25 @@ function operate(firstNumber, secondNumber, operator, mathFunc){
     console.log(add(firstNumber, secondNumber))
 }
 
+function typeing(){
+    let btns = document.querySelectorAll("button");
+    let display = document.querySelector("#display #dispContent");
+
+    btns.forEach((btn) =>{
+
+        btn.addEventListener("click", () =>{
+                 
+            display.textContent += btn.textContent;
+            displayValue = Array(display.textContent);
+            for(var i = 0; i < displayValue.length; ++i)
+            displayValue[i] = displayValue[i].replace(/(\r\n|\n|\r|\s)/gm,"")
+            displayValue = Number(displayValue);
+
+            console.log(displayValue);
+
+        })
+    })
+}
+
+typeing();
 operate(3,3,"/",add);
