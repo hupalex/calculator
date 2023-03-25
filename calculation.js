62dc432a275220c991f733c28a223a65c8d69c6c
hupalex <alex.hupuczi@gmail.com>
2023-03-25 17:02:07 +0100

    Implemented functionality to add/substract/divide/multiply numbers and continue these functions with the sum itself

--- calculation.js
+++ calculation.js
@@ -1,52 +1,79 @@
-let firstNumber;
-let operator;
-let secondNumber;
+let mathArray = [];
+let displayValue;
+let display = document.querySelector("#display #dispContent");
+let btns = document.querySelectorAll(".number");
 
-let displayValue
-
-function add(a,b){
-    return a+b;
-}
-
-function substract(a,b){
-    return a - b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
-}
 
 //A function that takes an operator, two numbers adn calls one of the math functions above
-function operate(firstNumber, secondNumber, operator, mathFunc){
-    operator = ["+","-","*","/"];
-    mathFunc = add();
-
-    console.log(add(firstNumber, secondNumber))
+function operate(firstNumber, secondNumber, operator){
+    let sum=`${firstNumber}${operator}${secondNumber}`;
+    return eval(sum);
 }
 
-function typeing(){
-    let btns = document.querySelectorAll("button");
-    let display = document.querySelector("#display #dispContent");
-
+function getFirstNumber(){
+    
     btns.forEach((btn) =>{
-
-        btn.addEventListener("click", () =>{
-                 
+        btn.addEventListener("click", () =>{         
             display.textContent += btn.textContent;
             displayValue = Array(display.textContent);
             for(var i = 0; i < displayValue.length; ++i)
             displayValue[i] = displayValue[i].replace(/(\r\n|\n|\r|\s)/gm,"")
             displayValue = Number(displayValue);
-
-            console.log(displayValue);
-
+            number = displayValue;
+        
         })
     })
 }
 
+function checkIfExist(arrayNum,equalto){
+    if(!mathArray[arrayNum]){
+        mathArray.push(equalto);
+    }else{
+        mathArray[arrayNum] = equalto;
+    }
+}
+
+function typeing(){
+    let ops = document.querySelectorAll(".operators");
+    
+    getFirstNumber();
+
+    ops.forEach((op) =>{
+        op.addEventListener("click", () =>{       
+            if(!mathArray.length){
+                mathArray.push(number);
+            }else {
+                checkIfExist(2,number)
+            }
+            
+            display.textContent ="";
+
+            if(mathArray[0]){
+                if(op.id==="+"){
+                    checkIfExist(1,op.id)
+                }else if(op.id==="-"){
+                    checkIfExist(1,op.id)
+                }else if(op.id==="*"){
+                    checkIfExist(1,op.id)
+                }else if(op.id==="/"){
+                    checkIfExist(1,op.id)
+                }else if(op.id ==="="){
+                    fn=mathArray[0]
+                    o=mathArray[1];
+                    sn=mathArray[2];
+                    
+                    display.textContent = operate(fn,sn,o);
+                    
+                    mathArray[0] = operate(fn,sn,o);
+                    
+                }         
+            }
+
+        })
+    })
+
+}
+
+
 typeing();
-operate(3,3,"/",add);
+
