f1c8c00d3182691ef6787f440549e45246c10556
hupalex <alex.hupuczi@gmail.com>
2023-03-25 20:02:54 +0100

    Updated function so users are able to chain math functions and then evaluate.

--- calculation.js
+++ calculation.js
@@ -4,14 +4,14 @@ let display = document.querySelector("#display #dispContent");
 let btns = document.querySelectorAll(".number");
 
 
-//A function that takes an operator, two numbers adn calls one of the math functions above
+//A function that takes an operator, two numbers and evaluates them
 function operate(firstNumber, secondNumber, operator){
     let sum=`${firstNumber}${operator}${secondNumber}`;
-    return eval(sum);
+    return eval(sum).toFixed(8);
 }
 
-function getFirstNumber(){
-    
+//A function that lets the user put the number on the display using numeric buttons
+function getNumber(){
     btns.forEach((btn) =>{
         btn.addEventListener("click", () =>{         
             display.textContent += btn.textContent;
@@ -20,11 +20,11 @@ function getFirstNumber(){
             displayValue[i] = displayValue[i].replace(/(\r\n|\n|\r|\s)/gm,"")
             displayValue = Number(displayValue);
             number = displayValue;
-        
         })
     })
 }
 
+//A function I created for refactoring purposes, checks if the used array has any element on a given number.
 function checkIfExist(arrayNum,equalto){
     if(!mathArray[arrayNum]){
         mathArray.push(equalto);
@@ -33,42 +33,49 @@ function checkIfExist(arrayNum,equalto){
     }
 }
 
+function checkAndOperate(id){
+    if(!mathArray[1]){
+        mathArray.push(id);
+    }else{
+        fn=mathArray[0]
+        o=mathArray[1];
+        sn=mathArray[2]; 
+        mathArray[0] = operate(fn,sn,o);
+        mathArray[1] = id;       
+    }
+}
+
+
 function typeing(){
     let ops = document.querySelectorAll(".operators");
-    
-    getFirstNumber();
 
+    getNumber();
     ops.forEach((op) =>{
         op.addEventListener("click", () =>{       
             if(!mathArray.length){
                 mathArray.push(number);
             }else {
                 checkIfExist(2,number)
-            }
-            
+            }   
+
             display.textContent ="";
 
             if(mathArray[0]){
                 if(op.id==="+"){
-                    checkIfExist(1,op.id)
+                   checkAndOperate(op.id);
                 }else if(op.id==="-"){
-                    checkIfExist(1,op.id)
+                    checkAndOperate(op.id);
                 }else if(op.id==="*"){
-                    checkIfExist(1,op.id)
+                    checkAndOperate(op.id);
                 }else if(op.id==="/"){
-                    checkIfExist(1,op.id)
+                    checkAndOperate(op.id);
                 }else if(op.id ==="="){
-                    fn=mathArray[0]
+                    fn=mathArray[0];
                     o=mathArray[1];
-                    sn=mathArray[2];
-                    
-                    display.textContent = operate(fn,sn,o);
-                    
-                    mathArray[0] = operate(fn,sn,o);
-                    
+                    sn=mathArray[2];      
+                    display.textContent = operate(fn,sn,o);  
                 }         
             }
-
         })
     })
 
